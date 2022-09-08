bffd48c8c8e36e8c8f44e5d8a17f1dce78230a23
PalarmAlex <instrpro@mail.ru>
2022-09-08 20:47:47 +0600

    результат импорта в окне диалога

--- tools/tools.php
+++ tools/tools.php
@@ -95,7 +95,7 @@ echo "</div>
 		function result_impr_larv(res) {
 			tools_action_ID = 0;
 			if (res != "yes") {
-				show_dlg_alert("Возникли проблемы при обновлении", 0);
+				show_dlg_alert(res, 0);
 				return;
 			}
 			show_dlg_alert("Все файлы успешно обновлены", 0);
