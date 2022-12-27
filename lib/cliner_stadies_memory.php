f5b1031504dc580fc3024546a80b001deda6f661
Алексей Парусников <instrpro@mail.ru>
2022-12-27 11:19:17 +0600

    коррекция переходов стадий - возврат через стадию

--- lib/cliner_stadies_memory.php
+++ lib/cliner_stadies_memory.php
@@ -15,39 +15,62 @@ mb_internal_encoding("UTF-8");
 
 $next_level=$_GET['next_level'];
 
+// допускается возврат, но запрещен переход вперед через стадию
 switch ($next_level){
   case 0: // до рождения
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/reflex_tree.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/condition_reflexes.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/trigger_stimuls_images.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/base_style_images.txt");
+    cliner_date_block(0);
+    cliner_date_block(1);
+    cliner_date_block(2);
+    cliner_date_block(4);
     break;
   case 1: // после рождения
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/condition_reflexes.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/trigger_stimuls_images.txt");
+    cliner_date_block(1);
+    cliner_date_block(2);
+    cliner_date_block(4);
     break;
   case 2: // автоматизмы
-  case 3:
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/automatizm_images.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/automatizm_tree.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/action_images.txt");
-    cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/verbal_images.txt");
+    cliner_date_block(2);
+    cliner_date_block(4);
+    break;   
+  case 3: // автоматизмы
+    cliner_date_block(4);
     break;
 }
-cliner_all_psy();
 
-function cliner_all_psy(){
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/cerebellum_reflex.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/trigger_and_actions.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/action_images_mental.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/episod_memory.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/goNext.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/importance.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/purpose_images.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/rules.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/situation_images.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/trigger_and_actions_mental.txt");
-  cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/understanding_tree.txt");
+function cliner_date_block($cur_level){
+  switch ($cur_level){
+    case 0: // базовые прошивки
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/reflex_tree.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/condition_reflexes.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/trigger_stimuls_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/base_style_images.txt");
+      break;
+    case 1: // рефлексы
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/condition_reflexes.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_reflex/trigger_stimuls_images.txt");
+      break;
+    case 2: // автоматизмы
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/automatizm_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/automatizm_tree.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/action_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/verbal_images.txt");
+      break;
+    case 4: // психика
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/cerebellum_reflex.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/trigger_and_actions.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/action_images_mental.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/episod_memory.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/goNext.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/importance.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/purpose_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/rules.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/situation_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/trigger_and_actions_mental.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/understanding_tree.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/activity_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/emotion_images.txt");
+      cliner_file($_SERVER["DOCUMENT_ROOT"]."/memory_psy/mental_automatizm_images.txt");
+  }
 }
 
 ///////////////////////////////////////////////////
